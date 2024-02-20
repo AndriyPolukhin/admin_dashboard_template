237827698b5756b991c641c1d691c0ec97b2d0fb
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-02-21 00:10:41 +0200

    Added theme.js with styles to be used for UI, added template image assets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { createTheme } from '@mui/material'
+import { themeSettings } from './theme'
+import { useMemo } from 'react'
+
 const App = () => {
+	const mode = 'dark'
+	const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
 	return (
 		<>
 			<div className='app'>
-				<p>Admin Dashboard Tempalte</p>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+				</ThemeProvider>
 			</div>
 		</>
 	)

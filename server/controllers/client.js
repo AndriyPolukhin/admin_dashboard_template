af0eba8321bca4aafedf63591cd7f17540e519d3
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-02-24 23:41:01 +0200

    Added getTransactions ednpoint to the api

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -36,17 +36,17 @@ export const getCustomers = async (req, res) => {
 export const getTransactions = async (req, res) => {
 	try {
 		// sort should look like this: { "field": "userId", "sort": "desc"}
-		const { page = 1, pageSize = 28, sort = null, search = '' } = req.query
+		const { page = 1, pageSize = 20, sort = null, search = '' } = req.query
 
-		// formatted sort should look like = { userId: -1}
+		// formatted sort should look like { userId: -1 }
 		const generateSort = () => {
 			const sortParsed = JSON.parse(sort)
 			const sortFormatted = {
 				[sortParsed.field]: (sortParsed.sort = 'asc' ? 1 : -1),
 			}
+
 			return sortFormatted
 		}
-
 		const sortFormatted = Boolean(sort) ? generateSort() : {}
 
 		const transactions = await Transaction.find({
